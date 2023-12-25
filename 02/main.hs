8cf19df811766bcfa694d67a6fd9ad4e8dc6b006
Pavel Kucera <pavel.kucera@hey.com>
2023-12-25 17:42:37 +0100

    feat(02): do a sweep with comments and better names

--- 02/main.hs
+++ 02/main.hs
@@ -28,25 +28,30 @@ data Game
 
 type Input = Parsec Void String
 
-isValidGame :: Bag -> Game -> Bool
-isValidGame b g = allValid rounds
+-- Check if game is possible with the given bag
+isPossibleGame :: Bag -> Game -> Bool
+isPossibleGame b g = allValid rounds
   where
     allValid = and . map isValidRound
     rounds = gameRounds g
     isValidRound round = M.isSubmapOfBy (<=) round b
 
+-- Get specs of a bag `b` with the minimum number of cubes of each color such
+-- that `isPossibleGame (minPossibleBag game) == True
 minPossibleBag :: Game -> Bag
 minPossibleBag = foldr (M.unionWith max) M.empty . gameRounds
 
+-- The power of a set of cubes is equal to the numbers of red, green, and blue
+-- cubes multiplied together
 bagPower :: Bag -> Int
 bagPower = M.foldr (*) 1
 
--- Use a parser and "eat" following whitespace
+-- Use a parser and "eat" following spaces; use carefully as the grammar does
+-- not allow for a lot of spaces
 lexeme :: Input a -> Input a
 lexeme p = p <* space
   where
     space = skipMany . satisfy $ (== ' ')
-
 
 symbol :: String -> Input ()
 symbol s = lexeme $ string s >> pure ()
@@ -56,11 +61,15 @@ symbol s = lexeme $ string s >> pure ()
 keyword :: String -> Input ()
 keyword = symbol
 
+-- Parse a positive number allowing for leading zeros
 number :: Input Int
 number = lexeme $ read <$> some digitChar
 
-cubeInput :: Input (Color, Int)
-cubeInput = do 
+-- Parse number + color
+-- 3 green
+-- 50 red
+cubeCount :: Input (Color, Int)
+cubeCount = do 
   count <- number
   color <- color
   pure (color, count)
@@ -70,11 +79,14 @@ cubeInput = do
         <|> string "green" $> Green
         <|> string "red"   $> Red
 
+-- Parse cube counts separated by commas
+-- 
 -- If there are multiple ocurrences of the same color, the parser uses the last
 -- value rather than combining
 roundInput :: Input Bag
-roundInput = M.fromList <$> cubeInput `sepBy` (symbol ",")
+roundInput = M.fromList <$> cubeCount `sepBy` (symbol ",")
 
+-- Parse a line with a full game
 gameInput :: Input Game
 gameInput = do
   keyword "Game"
@@ -83,6 +95,7 @@ gameInput = do
   rounds <- roundInput `sepBy` (symbol ";")
   pure $ Game id rounds
 
+-- Parse full input (require eof)
 input :: Input [Game]
 input = do
   games <- lexeme $ gameInput `sepEndBy` (newline)
@@ -90,6 +103,7 @@ input = do
   eof
   pure games
 
+-- Parse games, report errors. Not great, but will do for Advent of Code.
 parseGames :: String -> IO [Game]
 parseGames def = do
   case parse input "stdin" def of
@@ -103,7 +117,7 @@ main = do
   stdin <- getContents
   games <- parseGames stdin
 
-  let validGames = filter (isValidGame bagWithCubes) games
+  let validGames = filter (isPossibleGame bagWithCubes) games
   let validGamesIds = map gameId validGames
   let validGamesIdsSum = sum validGamesIds
   putStrLn . ("Sum of IDs of possible games: " ++) . show $ validGamesIdsSum
