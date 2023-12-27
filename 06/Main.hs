2cd086645260da186c483d01ea2d2cd1f9a591f9
Pavel Kucera <pavel.kucera@hey.com>
2023-12-27 18:46:56 +0100

    feat(06): implement faster, quadratic-inequality based solution

--- 06/Main.hs
+++ 06/Main.hs
@@ -28,6 +28,24 @@ winsRace race ms
 solutions :: Race -> [Millisecond]
 solutions race = filter (winsRace race) [1 .. (raceTime race)]
 
+-- Quadratic inequality-based solution
+-- assumes that there are always two solutions based on the nature of the
+-- exercise
+solve a b c =
+  let s1 = ((-b) - sqrt (b ^ 2 - 4 * a * c)) / (2 * a)
+      s2 = ((-b) + sqrt (b ^ 2 - 4 * a * c)) / (2 * a)
+   in (ceiling (min s1 s2), floor (max s1 s2))
+
+-- Returns the first and last "times to charge the boat" that would lead to
+-- winning a race
+--
+-- The "physics" leads to the following quadratic inequality
+-- x is the amount of milliseconds we use for "charging" the boat
+-- x*(time - x) > distance
+-- -x^2 + time * x - distance > 0
+solveRace (Race time distance) =
+  solve (-1) (realToFrac time) (-realToFrac distance)
+
 main :: IO ()
 main = do
   let races =
@@ -38,3 +56,8 @@ main = do
         ]
   let solutionSizes = product . map (length . solutions) $ races
   print solutionSizes
+
+  let bigRace = Race 63789468 411127420471035
+      (s1, s2) = solveRace bigRace
+  -- inclusive interval, need to add 1 solution
+  print (s2 - s1 + 1)
