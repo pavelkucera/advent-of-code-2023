2d07268bb354fc14a5fa56eda15970951795fb8f
Pavel Kucera <pavel.kucera@hey.com>
2023-12-27 18:18:59 +0100

    feat(06): implement part 1

--- 06/Main.hs
+++ 06/Main.hs
@@ -1,5 +1,40 @@
 module Main where
 
+type Millisecond = Int
+
+type Millimeter = Int
+
+-- Speed in millimeters per milliseconds
+type Speed = Int
+
+data Race = Race
+  { raceTime :: Millisecond,
+    raceRecordDistance :: Millimeter
+  }
+  deriving (Show)
+
+charge :: Millisecond -> Speed
+charge ms = ms
+
+winsRace :: Race -> Millisecond -> Bool
+winsRace race ms
+  | ms > raceTime race = False
+  | otherwise =
+      distanceTravelled > raceRecordDistance race
+  where
+    distanceTravelled = charge ms * (raceTime race - ms)
+
+-- Brute-force solution instead of solving the underlying quadratic inequality
+solutions :: Race -> [Millisecond]
+solutions race = filter (winsRace race) [1 .. (raceTime race)]
+
 main :: IO ()
 main = do
-  putStrLn "Day 06"
+  let races =
+        [ Race 63 411,
+          Race 78 1274,
+          Race 94 2047,
+          Race 68 1035
+        ]
+  let solutionSizes = product . map (length . solutions) $ races
+  print solutionSizes
