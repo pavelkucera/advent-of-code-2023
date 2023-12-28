4d239939bd193e337932bd1f9d22c953dbfcd974
Pavel Kucera <pavel.kucera@hey.com>
2023-12-28 08:42:56 +0100

    feat(08): implement part 1

--- 08/Main.hs
+++ 08/Main.hs
@@ -1,7 +1,69 @@
+{-# LANGUAGE ImportQualifiedPost #-}
+
 module Main where
 
 import Data.Functor (($>))
+import Data.Map (Map)
+import Data.Map.Strict qualified as M
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char
+
+data Instruction
+  = L
+  | R
+  deriving (Eq, Show)
+
+type Input = Parsec Void String
+
+type NetworkMap = Map String (String, String)
+
+stepCount :: NetworkMap -> [Instruction] -> Int
+stepCount network baseInstructions = go "AAA" (cycle baseInstructions) 0
+  where
+    go "ZZZ" _ c = c
+    go node (i : is) c =
+      go (choose i $ network M.! node) is (c + 1)
+    choose L = fst
+    choose R = snd
+
+networkMapP :: Input ([Instruction], NetworkMap)
+networkMapP = do
+  is <- many instructionP
+  some eol
+  nodes <- nodeP `sepEndBy` eol
+  eof
+  pure (is, M.fromList nodes)
+
+instructionP :: Input Instruction
+instructionP =
+  choice
+    [ char 'L' $> L,
+      char 'R' $> R
+    ]
+
+nameP :: Input String
+nameP = count 3 alphaNumChar
+
+nodeP :: Input (String, (String, String))
+nodeP = do
+  n <- lexeme nameP
+  symbol "="
+  symbol "("
+  l <- lexeme nameP
+  symbol ","
+  r <- lexeme nameP
+  symbol ")"
+  pure (n, (l, r))
+  where
+    lexeme p = p <* many (char ' ')
+    symbol s = lexeme $ string s
 
 main :: IO ()
-main =
-  putStrLn "Day 08"
+main = do
+  stdin <- getContents
+  (instructions, networkMap) <- case parse networkMapP "stdin" stdin of
+    Left e -> fail . errorBundlePretty $ e
+    Right nm -> pure nm
+
+  print $ stepCount networkMap instructions
