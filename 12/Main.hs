af09f907a1249da31db113f0ce913627880128d6
Pavel Kucera <pavel.kucera@hey.com>
2023-12-30 23:22:51 +0100

    feat(12): implement first functions

--- 12/Main.hs
+++ 12/Main.hs
@@ -1,5 +1,35 @@
 module Main where
 
+import Data.List (group)
+
+data Spring
+  = Operational
+  | Damaged
+  | Unknown
+  deriving (Eq, Show)
+
+readRecord :: String -> [Spring]
+readRecord = map spring
+  where
+    spring '?' = Unknown
+    spring '.' = Operational
+    spring '#' = Damaged
+
+-- Stupidly generates all the possible replacements of unknown states. A
+-- smarter approach would consider current state and the rules. It e.g
+-- does not make sense to keep generating a possibility if we have already
+-- generated too many broken states.
+possibilities :: [Spring] -> [[Spring]]
+possibilities =
+  foldr combine [[]]
+  where
+    combine Unknown ss = map (Operational :) ss ++ map (Damaged :) ss
+    combine s ss = map (s :) ss
+
+fitsRecord :: [Int] -> [Spring] -> Bool
+fitsRecord brokenCounts records =
+  map length (filter (all (== Damaged)) $ group records) == brokenCounts
+
 main :: IO ()
 main = do
   putStrLn "day 12"
