3c70456ee0266e65815fed6a19d9ad8265fabbf7
Pavel Kucera <pavel.kucera@hey.com>
2023-12-31 07:56:38 +0100

    feat(12): implement part one

--- 12/Main.hs
+++ 12/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Data.Char (isDigit, isSpace)
 import Data.List (group)
 
 data Spring
@@ -8,12 +9,32 @@ data Spring
   | Unknown
   deriving (Eq, Show)
 
+readRecords :: String -> [([Spring], [Int])]
+readRecords = map readRecordLine . lines
+
 readRecord :: String -> [Spring]
 readRecord = map spring
   where
     spring '?' = Unknown
     spring '.' = Operational
     spring '#' = Damaged
+
+readRecordLine :: String -> ([Spring], [Int])
+readRecordLine s =
+  let (record, counts) = break (== ' ') s
+   in (readRecord record, map (read . trim) $ splitOn counts ',')
+
+splitOn :: String -> Char -> [String]
+splitOn [] _ = []
+splitOn text delimiter
+  | null text = []
+  | otherwise =
+      first : splitOn (drop 1 rest) delimiter
+  where
+    (first, rest) = break (== delimiter) text
+
+trim :: String -> String
+trim = reverse . dropWhile isSpace . reverse . dropWhile isSpace
 
 -- Stupidly generates all the possible replacements of unknown states. A
 -- smarter approach would consider current state and the rules. It e.g
@@ -26,10 +47,16 @@ possibilities =
     combine Unknown ss = map (Operational :) ss ++ map (Damaged :) ss
     combine s ss = map (s :) ss
 
-fitsRecord :: [Int] -> [Spring] -> Bool
-fitsRecord brokenCounts records =
+fitsCounts :: [Int] -> [Spring] -> Bool
+fitsCounts brokenCounts records =
   map length (filter (all (== Damaged)) $ group records) == brokenCounts
+
+arrangementCount :: [Spring] -> [Int] -> Int
+arrangementCount record counts = length . filter (fitsCounts counts) $ possibilities record
 
 main :: IO ()
 main = do
-  putStrLn "day 12"
+  stdin <- getContents
+  let records = readRecords stdin
+      arrangementCounts = sum . map (uncurry arrangementCount) $ records
+  print arrangementCounts
